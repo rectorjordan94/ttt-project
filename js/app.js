3f8db66f25f25a43b16c1aa58394a87f203c8d29
rectorjordan94 <rectorjordan94@gmail.com>
2022-12-18 22:59:27 -0600

    added additional styling based on win condition

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// variable keeps track of moves, true = X, false = O
 let moveCounter = 0
 let gameEnd = false
 let winner = ''
@@ -16,6 +15,7 @@ const winSix = ['box-3', 'box-6', 'box-9']
 const winSeven = ['box-1', 'box-5', 'box-9']
 const winEight = ['box-3', 'box-5', 'box-7']
 const winConditions = [winOne, winTwo, winThree, winFour, winFive, winSix, winSeven, winEight]
+const boxes = document.querySelectorAll('.box')
 
 const isInCombo = (array1, array2) => {
     const checkFor = (box) => {
@@ -37,7 +37,7 @@ const randomizeColors = () => {
 let colors = randomizeColors()
 title.style.textShadow = `2px 2px 10px ${colors[0]}`;
 subtitle.style.textShadow = `2px 2px 10px ${colors[1]}`;
-const boxes = document.querySelectorAll('.box')
+
 
 const resetGrid = () => {
     colors = randomizeColors()
@@ -45,7 +45,10 @@ const resetGrid = () => {
     subtitle.style.textShadow = `2px 2px 10px ${colors[1]}`;
     for (let i = 0; i < 9; i++){
         boxes[i].classList.remove('hasBeenClicked')
-        boxes[i].style.backgroundColor = 'rgb(52, 52, 52)'
+        boxes[i].style.background = `radial-gradient(circle at 150px 150px, rgb(255,255,255), rgb(0,0,0))`
+        boxes[i].style.transform = ''
+        boxes[i].innerHTML = ''
+        boxes[i].style.opacity = 1
         playerOneMoves.pop()
         playerTwoMoves.pop()
     }
@@ -56,18 +59,30 @@ const makeMove = (event) => {
     const selected = event.target
     moveCounter++
     if (moveCounter % 2 !== 0 && moveCounter !== 9) {
-        selected.style.backgroundColor = colors[0]
+        selected.style.background = `radial-gradient(circle at 150px 150px, ${colors[0]}, rgb(0,0,0))`
         selected.classList.add('hasBeenClicked')
         playerOneMoves.push(selected.id)
-        console.log(playerOneMoves)
     } else if (moveCounter % 2 === 0) {
-        selected.style.backgroundColor = colors[1]
+        selected.style.background = `radial-gradient(circle at 150px 150px, ${colors[1]}, rgb(0,0,0))`
         selected.classList.add('hasBeenClicked')
         playerTwoMoves.push(selected.id)
     } else if (moveCounter === 9) {
-        selected.style.backgroundColor = colors[0]
-        console.log('its a tie')
+        selected.style.background = `radial-gradient(circle at 150px 150px, ${colors[0]}, rgb(0,0,0))`
         playerOneMoves.push(selected.id)
+        for (let i = 0; i < 9; i++){
+            const middle = boxes[4]
+            if (i % 2 === 0) {
+                boxes[i].style.background = `radial-gradient(circle at 150px 150px, ${colors[0]}, rgb(0,0,0))`
+            } else {
+                boxes[i].style.background = `radial-gradient(circle at 150px 150px, ${colors[1]}, rgb(0,0,0))`
+            }
+            if (boxes[i].id === 'box-5') {
+                middle.style.transform = 'scale(3, 3)'
+                middle.innerHTML = 'TIE'
+            } else {
+                boxes[i].style.opacity = '0'
+            }
+        }
     }
     checkWin()
 }
@@ -76,16 +91,29 @@ const checkWin = () => {
     for(i = 0; i < winConditions.length; i++) {
         if (isInCombo(playerOneMoves, winConditions[i]) === true) {
             for (let i = 0; i < 9; i++){
-                boxes[i].style.backgroundColor = colors[0]
+                boxes[i].style.background = `radial-gradient(circle at 150px 150px, ${colors[0]}, rgb(0,0,0))`
+                const middle = boxes[4]
+                if (boxes[i].id === 'box-5') {
+                    middle.style.transform = 'scale(3, 3)'
+                    middle.innerHTML = 'PLAYER ONE WINS'
+                } else {
+                    boxes[i].style.opacity = '0'
+                }
             }
             gameOver()
-            return console.log('Player One Wins!')
         } else if (isInCombo(playerTwoMoves, winConditions[i]) === true) {
             for (let i = 0; i < 9; i++){
-                boxes[i].style.backgroundColor = colors[1]
+                boxes[i].style.background = `radial-gradient(circle at 150px 150px, ${colors[1]}, rgb(0,0,0))`
+                const middle = boxes[4]
+                if (boxes[i].id === 'box-5') {
+                    middle.style.transform = 'scale(3, 3)'
+                    middle.innerHTML = 'PLAYER TWO WINS'
+                } else {
+                    boxes[i].style.opacity = '0'
+                }
+                
             }
             gameOver()
-            return console.log('Player Two Wins!')
         }
     }
 }
@@ -95,7 +123,6 @@ const gameOver = () => {
         boxes[i].classList.add('hasBeenClicked')
     }
 }
-
 
 for (let i = 0; i < 9; i++){
     boxes[i].addEventListener('click', makeMove)
